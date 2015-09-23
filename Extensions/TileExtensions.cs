77fca38bf908b90b9845e40a538060bb5e5601df
Thomas GERVAIS <thomas.gervais@st.com>
2015-09-23 10:07:27 +0200

    [Add] T must be class

--- Extensions/TileExtensions.cs
+++ Extensions/TileExtensions.cs
@@ -8,19 +8,19 @@ namespace PA.TileList.Extensions
     public static class TileExtensions
     {
         public static Tile<T> ToTile<T>(this IEnumerable<T> c, int referenceIndex = 0)
-            where T : ICoordinate
+            where T : class, ICoordinate
         {
             return new Tile<T>(c, referenceIndex);
         }
 
         public static ITile<T> AsTile<T>(this IEnumerable<T> l, int referenceIndex = 0)
-            where T : ICoordinate
+            where T : class, ICoordinate
         {
             return l as ITile<T> ?? l.ToTile(referenceIndex);
         }
 
         public static ITile<T> AsTile<T>(this IEnumerable<T> l, IArea a, int referenceIndex = 0)
-            where T : ICoordinate
+            where T : class, ICoordinate
         {
             return l as ITile<T> ?? l.ToTile(referenceIndex);
         }
