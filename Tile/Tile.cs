c9ee795301489838c96e53ed9dd04c1a274c6aa5
Thomas GERVAIS <thomas.gervais@st.com>
2015-09-23 10:07:12 +0200

    [Chg] Fill behavior [Add] T must be class

--- Tile/Tile.cs
+++ Tile/Tile.cs
@@ -6,7 +6,8 @@ using System.Collections;
 
 namespace PA.TileList
 {
-    public class Tile<T> : List<T>, ITile<T> where T : ICoordinate
+    public class Tile<T> : List<T>, ITile<T> 
+    where T : class, ICoordinate
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -92,7 +93,7 @@ namespace PA.TileList
 
         public List<T> FindAll(IArea a)
         {
-            return base.FindAll(e => a.Contains(e));
+            return base.FindAll(a.Contains);
         }
 
         public void Remove(int x, int y)
@@ -102,7 +103,7 @@ namespace PA.TileList
 
         public void RemoveAll(IArea a)
         {
-            this.RemoveAll(e => a.Contains(e));
+            this.RemoveAll(a.Contains);
         }
 
         public void UpdateArea()
@@ -111,39 +112,34 @@ namespace PA.TileList
         }
 
         /// <summary>
-        /// Ask for all non assigned elements
+        /// Fill area  with specified filler.
         /// </summary>
-        /// <param name="SizeX"></param>
-        /// <param name="SizeY"></param>
-        /// <param name="filler"></param>
-        /// <param name="ShiftX"></param>
-        /// <param name="ShiftY"></param>
-        public void Fill(ushort SizeX, ushort SizeY, Func<Coordinate, T> filler, decimal ShiftX = 0, decimal ShiftY = 0)
+        /// <param name="filler">Filler.</param>
+        /// <param name="overwrite">Overwrite existing element</param>
+        public void Fill(Func<Coordinate, T> filler, bool overwrite = false)
         {
-            int StartX = Math.Min(this.Area.Max.X, Math.Max(this.Area.Min.X, Convert.ToInt32(ShiftX - SizeX / 2m)));
-            int StartY = Math.Min(this.Area.Max.Y, Math.Max(this.Area.Min.Y, Convert.ToInt32(ShiftY - SizeY / 2m)));
-
-            Area a = new Area(StartX, StartY, StartX + SizeX - 1, StartY + SizeY - 1);
-
-            this.Fill(filler, a);
+            this.Fill(this.Area, filler, overwrite);
         }
 
 
         /// <summary>
-        /// Ask for all non assigned elements in specified Area
+        /// Fill area  with specified filler.
         /// </summary>
-        /// <param name="SizeX"></param>
-        /// <param name="SizeY"></param>
-        /// <param name="filler"></param>
-        /// <param name="ShiftX"></param>
-        /// <param name="ShiftY"></param>
-        public void Fill(Func<Coordinate, T> filler, IArea a = null)
+        /// <param name="area">Area.</param>
+        /// <param name="filler">Filler.</param>
+        /// <param name="overwrite">Overwrite existing element</param>
+        public void Fill(IArea area, Func<Coordinate, T> filler, bool overwrite = false)
         {
-            Area area = new Area(a ?? this.Area);
-
             foreach (Coordinate c in area)
             {
-                if (!this.Any(t => t.X == c.X && t.Y == c.Y))
+                T item = this.FirstOrDefault(t => t.X == c.X && t.Y == c.Y);
+
+                if (item != null && overwrite)
+                {
+                    this.Remove(item);
+                }
+
+                if (item == null || overwrite)
                 {
                     this.Add(filler(c));
                 }
@@ -151,6 +147,25 @@ namespace PA.TileList
 
             this.TrimExcess();
             this.UpdateArea();
+        }
+
+        /// <summary>
+        /// Fill zone with specified filler.
+        /// </summary>
+        /// <param name="SizeX">Size to fill</param>
+        /// <param name="SizeY">Size to fill</param>
+        /// <param name="filler"></param>
+        /// <param name="ShiftX">Shift regarding area center</param>
+        /// <param name="ShiftY">Shift regarding area center</param>
+        /// <param name="overwrite">Overwrite existing element</param>
+        public void Fill(ushort SizeX, ushort SizeY, Func<Coordinate, T> filler, decimal ShiftX = 0, decimal ShiftY = 0, bool overwrite = false)
+        {
+            int StartX = Math.Min(this.Area.Max.X, Math.Max(this.Area.Min.X, Convert.ToInt32(ShiftX - SizeX / 2m)));
+            int StartY = Math.Min(this.Area.Max.Y, Math.Max(this.Area.Min.Y, Convert.ToInt32(ShiftY - SizeY / 2m)));
+
+            var area = new Area(StartX, StartY, StartX + SizeX - 1, StartY + SizeY - 1);
+
+            this.Fill(area, filler, overwrite);
         }
 
         public IEnumerable<T> Inside(IArea a)
