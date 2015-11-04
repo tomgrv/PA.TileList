9adec945b30e20181f463d0512cd03e75a4d6b25
Thomas GERVAIS <thomas.gervais@st.com>
2015-11-04 11:20:47 +0100

    [Add] Checksum

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -4,117 +4,136 @@ using System.Linq;
 
 namespace PA.TileList.Extensions
 {
-    public static class ListExtensions
-    {
-        public static IEnumerable<T> WhereOrDefault<T>(this IEnumerable<T> list, Func<T, bool> predicate)
-        {
-            return predicate != null ? list.Where(predicate) : list.AsEnumerable();
-        }
+	public static class ListExtensions
+	{
+		public static IEnumerable<T> WhereOrDefault<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+		{
+			return predicate != null ? list.Where(predicate) : list.AsEnumerable();
+		}
 
-        #region Crop
+		#region Crop
 
-        #region IEnumerable
+		#region IEnumerable
 
-        public static IEnumerable<T> Crop<T>(this IEnumerable<T> list, IArea a)
+		public static IEnumerable<T> Crop<T>(this IEnumerable<T> list, IArea a)
          where T : ICoordinate
-        {
-            return list.Where(e => a.Contains(e));
-        }
+		{
+			return list.Where(e => a.Contains(e));
+		}
 
-        public static IEnumerable<T> Crop<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+		public static IEnumerable<T> Crop<T>(this IEnumerable<T> list, Func<T, bool> predicate)
             where T : ICoordinate
-        {
-            return list.Crop(list.GetCropArea(predicate));
-        }
+		{
+			return list.Crop(list.GetCropArea(predicate));
+		}
 
-        #endregion
+		#endregion
 
-        #region ITile
+		#region ITile
 
-        public static ITile<T> Crop<T>(this ITile<T> list, IArea a)
+		public static ITile<T> Crop<T>(this ITile<T> list, IArea a)
          where T : ICoordinate
-        {
-            foreach (var e in list.Where(e => !a.Contains(e)).ToArray())
-            {
-                list.Remove(e);
-            }
+		{
+			foreach (var e in list.Where(e => !a.Contains(e)).ToArray())
+			{
+				list.Remove(e);
+			}
 
-            list.UpdateArea();
-            return list;
-        }
+			list.UpdateArea();
+			return list;
+		}
 
-        public static ITile<T> Crop<T>(this ITile<T> list, Func<T, bool> predicate)
+		public static ITile<T> Crop<T>(this ITile<T> list, Func<T, bool> predicate)
             where T : ICoordinate
-        {
-            return list.Crop(list.GetCropArea(predicate));
-        }
+		{
+			return list.Crop(list.GetCropArea(predicate));
+		}
 
-        #endregion
+		#endregion
 
-        #region IQuantifiedTile
+		#region IQuantifiedTile
 
-        public static IQuantifiedTile<T> Crop<T>(this IQuantifiedTile<T> list, IArea a)
+		public static IQuantifiedTile<T> Crop<T>(this IQuantifiedTile<T> list, IArea a)
          where T : ICoordinate
-        {
-            foreach (var e in list.Where(e => !a.Contains(e)).ToArray())
-            {
-                list.Remove(e);
-            }
-            list.UpdateArea();
-            return list;
-        }
+		{
+			foreach (var e in list.Where(e => !a.Contains(e)).ToArray())
+			{
+				list.Remove(e);
+			}
+			list.UpdateArea();
+			return list;
+		}
 
-        public static IQuantifiedTile<T> Crop<T>(this IQuantifiedTile<T> list, Func<T, bool> predicate)
+		public static IQuantifiedTile<T> Crop<T>(this IQuantifiedTile<T> list, Func<T, bool> predicate)
             where T : ICoordinate
-        {
-            return list.Crop(list.GetCropArea(predicate));
-        }
+		{
+			return list.Crop(list.GetCropArea(predicate));
+		}
 
-        #endregion
+		#endregion
 
-        internal static Area GetCropArea<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+		internal static Area GetCropArea<T>(this IEnumerable<T> list, Func<T, bool> predicate)
          where T : ICoordinate
-        {
-            // Crop area
-            Area crop = list.GetArea();
+		{
+			// Crop area
+			Area crop = list.GetArea();
 
-            // Reduce on x increasing
-            IEnumerable<T> l1 = list.Where(c => c.X == crop.Min.X);
-            while (l1.All(predicate))
-            {
-                crop.Min.X++;
-                l1 = list.Where(c => c.X == crop.Min.X);
-            }
+			// Reduce on x increasing
+			IEnumerable<T> l1 = list.Where(c => c.X == crop.Min.X);
+			while (l1.All(predicate))
+			{
+				crop.Min.X++;
+				l1 = list.Where(c => c.X == crop.Min.X);
+			}
 
-            // Reduce on x decreasing
-            IEnumerable<T> l2 = list.Where(c => c.X == crop.Max.X);
-            while (l2.All(predicate))
-            {
-                crop.Max.X--;
-                l2 = list.Where(c => c.X == crop.Max.X);
-            }
+			// Reduce on x decreasing
+			IEnumerable<T> l2 = list.Where(c => c.X == crop.Max.X);
+			while (l2.All(predicate))
+			{
+				crop.Max.X--;
+				l2 = list.Where(c => c.X == crop.Max.X);
+			}
 
-            // Reduce on y increasing, limit to x-cropping
-            IEnumerable<T> l3 = list.Where(c => c.Y == crop.Min.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
-            while (l3.All(predicate))
-            {
-                crop.Min.Y++;
-                l3 = list.Where(c => c.Y == crop.Min.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
-            }
+			// Reduce on y increasing, limit to x-cropping
+			IEnumerable<T> l3 = list.Where(c => c.Y == crop.Min.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
+			while (l3.All(predicate))
+			{
+				crop.Min.Y++;
+				l3 = list.Where(c => c.Y == crop.Min.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
+			}
 
-            // Reduce on y decreasing, limit to x-cropping
-            IEnumerable<T> l4 = list.Where(c => c.Y == crop.Max.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
-            while (l4.All(predicate))
-            {
-                crop.Max.Y--;
-                l4 = list.Where(c => c.Y == crop.Max.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
-            }
+			// Reduce on y decreasing, limit to x-cropping
+			IEnumerable<T> l4 = list.Where(c => c.Y == crop.Max.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
+			while (l4.All(predicate))
+			{
+				crop.Max.Y--;
+				l4 = list.Where(c => c.Y == crop.Max.Y && c.X >= crop.Min.X && c.X <= crop.Max.X);
+			}
 
-            return crop;
-        }
+			return crop;
+		}
 
-        #endregion
+		#endregion
 
+		public static string GetChecksum<T>(this IEnumerable<T> list, Func<T, IEnumerable<char>> signature)
+         where T : ICoordinate
+		{
+			int checksum = 0;
 
-    }
+			foreach (char c in list.SelectMany(signature))
+			{
+				checksum = ((checksum + c) * 16) % 251;
+			}
+
+			checksum = ((checksum + 'A') * 16) % 251;
+			checksum = ((checksum + 'A')) % 251;
+
+			if (checksum != 0)
+			{
+				checksum = 251 - checksum;
+			}
+
+			return (65 + (checksum / 16)).ToString() + (65 + (checksum % 16)).ToString();
+		}
+	}
 }
