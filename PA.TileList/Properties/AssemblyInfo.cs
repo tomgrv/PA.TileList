eea02fda87e7206b31af03e19345e63048fbc57b
Thomas GERVAIS <thomas.gervais@gmail.com>
2020-11-08 14:17:14 +0100

    feat!: move to net standard 2.x where possible

--- PA.TileList/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-
-[assembly: AssemblyTitle("PA.TileList")]
-[assembly: AssemblyDescription("Tile-organized, multi-level list managment (eg: xy object map, zoomable list, etc...)")]
